726944689fdc9ba1e6bc29eeb5155f7ccbb536a8
Maximilian Turza <maxturza@hotmail.com>
2022-09-26 23:33:11 +0200

    updated location to use current location

--- main.cpp
+++ main.cpp
@@ -4,6 +4,9 @@
 #include <filesystem>
 #include <string>
 #include <vector>
+#include <unistd.h>
+
+
 
 
 using namespace std;
@@ -32,6 +35,10 @@ int getDirectory(string dir, vector<string> &files, char* filename){
 
 int main(int argc, char** argv){
 
+    char currentLocation[256];
+
+    getcwd(currentLocation, 256);
+
     // usage ./main.cpp [absolute path] [filename] --> path must start at /home/.. for now, will be fixed later. 
     // usage should also be  ./myfind [absolute path] [filename] .... in later revisions
 
@@ -40,11 +47,17 @@ int main(int argc, char** argv){
     string dir = string("/home/ubuntu/dev/myfind2/test/");
     vector<string> files = vector<string>();
 
+    cout << "Current Location: " << currentLocation << endl;
+
+    string currentLocationString(currentLocation);
+    string currentFileString(argv[1]);
+
+
+    string finaldir = currentLocationString + currentFileString;
 
     string filename(argv[2]);
-    string directory(argv[1]);
     
-    getDirectory(directory, files, argv[2]);
+    getDirectory(finaldir, files, argv[2]);
 
 
     return 0;
