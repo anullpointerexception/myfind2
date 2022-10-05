8e8e4ab3c3c6e3ed64cd5b969858f450aa419097
Maximilian Turza <maxturza@hotmail.com>
2022-10-05 11:05:34 +0200

    implemented listFiles()

--- main.cpp
+++ main.cpp
@@ -7,61 +7,74 @@
 #include <unistd.h>
 #include <thread>
 #include <sys/wait.h>
+#include <filesystem>
 
 using namespace std;
+namespace fs = filesystem;
 
-int getDirectory(string dir, vector<string> &files, char* filename){
+bool recursive = false;
+bool case_insensitive = false;
 
-    DIR *dp;
-
-    struct dirent *dirEntry;
-
-    if((dp = opendir(dir.c_str())) == NULL){
-        cout << "Error(" << errno << ") opening " << dir << endl;
-        return errno;
-    }
-    while((dirEntry = readdir(dp)) != NULL){
-        string fname = dirEntry->d_name;
-        // cout << dirEntry->d_name;
-        if(fname.find(filename) != string::npos){
-            files.push_back(fname);
-            // cout << "File found!" << endl;
+void listFiles(string path, string filename)
+{
+    if (recursive)
+    {
+        if (case_insensitive)
+        {
+            // i dunno what here? -> all to lower case?
         }
-    }
-    closedir(dp);
-    return 0;
-}
-
-void listDirectoryRecursive(const string& location){
-    DIR *dp;
-    struct dirent *entry;
-
-    if((dp = opendir(location.c_str())) != NULL){
-        while((entry = readdir(dp)) != NULL){
-           if(string(entry->d_name).compare(".") != 0){
-                cout<< entry->d_name << endl;
-            } else {
-                string nextDir = string(entry -> d_name);
-                nextDir += "\\";
-
-                cout <<  nextDir << endl;
-
-                listDirectoryRecursive(nextDir);
+        else
+        {
+        }
+        for (auto const &dir_entry : fs::recursive_directory_iterator(path))
+        {
+            string name = fs::path(dir_entry).filename();
+            string extension = fs::path(dir_entry).extension();
+            // cout << "Filename: " << name << " Relative Path of File: " << fs::path(dir_entry).relative_path() << "\n";
+            // cout << "Filename dir: " << name << endl;
+            // cout << "Filename given " << filename << endl;
+            if (name == filename)
+            {
+                cout << "File " << filename << " found @ location " << fs::path(dir_entry).relative_path() << " using recursion " << endl;
+                //return 1;
+            }
+            else
+            {
+                //return -1;
             }
         }
     }
-    closedir(dp);
+    else
+    {
+        for (auto const &dir_entry : fs::directory_iterator(path))
+        {
+            string name = fs::path(dir_entry).filename();
+            cout << name << endl;
+            if (name == filename)
+            {
+                cout << "File " << filename << " found @ location " << fs::path(dir_entry).relative_path() << endl;
+                break;
+                //return 1;
+            }
+            else
+            {
+
+                cout << "File " << filename << " not found" << endl;
+                //return -1;
+            }
+        }
+    }
+    //return -2;
 }
 
+int main(int argc, char *argv[])
+{
 
-int main(int argc, char* argv[]){
-
-    bool recursive = false;
-    bool case_insensitive = false;
     string search_path;
     vector<string> files;
-    
-    if(argc<3){
+
+    if (argc < 3)
+    {
         cerr << "ERROR: missing parameters\nRequired parameters: searchpath, filename\nOptional parameters: additional filenames, \"-i\": insensitive search, \"-R\": search in subdirectories" << endl;
         return -1;
     }
@@ -69,41 +82,58 @@ int main(int argc, char* argv[]){
     int opt;
 
     //Flag handling
-    while((opt = getopt(argc, argv, ":iR")) != -1){
-        switch(opt){
-            case 'R':
-                if(recursive == true){
-                    cerr << "ERROR: Invalid flag!" << endl;
-                    return -1;
-                }
-                //cout << "switched to recursive mode" << endl;
-                recursive = true;
-                break;
-            case 'i':
-                if(case_insensitive == true){
-                    cerr << "ERROR: Invalid flag!" << endl;
-                    return -1;
-                }
-                //cout << "using case insensitive mode" << endl;
-                case_insensitive = true;
-                break;
-            case '?':
+    while ((opt = getopt(argc, argv, ":iR")) != -1)
+    {
+        switch (opt)
+        {
+        case 'R':
+            if (recursive == true)
+            {
                 cerr << "ERROR: Invalid flag!" << endl;
                 return -1;
+            }
+            cout << "switched to recursive mode" << endl;
+            recursive = true;
+
+            break;
+        case 'i':
+            if (case_insensitive == true)
+            {
+                cerr << "ERROR: Invalid flag!" << endl;
+                return -1;
+            }
+            //cout << "using case insensitive mode" << endl;
+            case_insensitive = true;
+            break;
+        case '?':
+            cerr << "ERROR: Invalid flag!" << endl;
+            return -1;
         }
     }
 
     search_path = argv[optind++];
     //check if valid searchpath
-    cout << "Searchpath:  " << search_path << endl;
-
-    for(;optind<argc;++optind){
+    for (; optind < argc; ++optind)
+    {
         files.push_back(argv[optind]);
     }
+    string pathVar = fs::current_path();
+    if (recursive == true)
+    {
+        string pathVar = fs::current_path();
+    }
+    else
+    {
+        cout << "Search path: " << search_path << endl;
+        pathVar = pathVar + search_path;
+    }
+    cout << "pathVar " << pathVar << endl;
 
-    for(unsigned int i=0;i<files.size();++i){
-        cout << "File " << i+1 << ": " << files[i] << endl;
+    for (unsigned int i = 0; i < files.size(); ++i)
+    {
+        cout << "File " << i + 1 << ": " << files[i] << endl;
         //start child process with a file each
+        listFiles(pathVar, files[i]);
     }
 
     return 0;
